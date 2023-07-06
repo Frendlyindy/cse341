a51ca969a035a1f218cbf96ea90be4122c3cd2ef
Frendlyindy <frendlyindian@gmail.com>
2023-07-06 14:41:12 -0600

    working on render

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Temple API',
   },
-  host: 'localhost:8080',
-  schemes: ['http'],
+  host: 'cse341-palmer.onrender.com',
+  schemes: ['http', 'https'],
 };
 
 const outputFile = './swagger.json';
